f588347c67fa227ffab671e625a369e254e28161
eric010101 <43527995+eric010101@users.noreply.github.com>
2019-02-26 14:54:01 +0800

    Update esbc_functions.py

--- esbc_functions.py
+++ esbc_functions.py
@@ -2,11 +2,11 @@
 import sys
 import socket
 def writefile(fname, fcontent, fstyle):
-        f = open(fname, "w")
-        f.write(fcontent)
-        # Close opend file
-        f.close()
-        return 1
+	f = open(fname, "w")
+	f.write(fcontent)
+	# Close opend file
+	f.close()
+	return 1
 def get_host_ip():
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,18 +17,18 @@ def get_host_ip():
 
 	return ip
 def inplace_change(filename, old_string, new_string,outfilename):
-    # Safely read the input filename using 'with'
-    with open(filename) as f:
-        s = f.read()
-        if old_string not in s:
-            print(old_string,"not found in ",filename)
-            #return
+	# Safely read the input filename using 'with'
+	with open(filename) as f:
+		s = f.read()
+		if old_string not in s:
+			print(old_string,"not found in ",filename)
+			#return
 
-    # Safely write the changed content, if found in the file
-    with open(outfilename, 'w') as f:
-        print('Changing ',old_string, ' to ',new_string,' in ',outfilename)
-        s = s.replace(old_string, new_string)
-        f.write(s)
+	# Safely write the changed content, if found in the file
+	with open(outfilename, 'w') as f:
+		print('Changing ',old_string, ' to ',new_string,' in ',outfilename)
+		s = s.replace(old_string, new_string)
+		f.write(s)
 
 def genesis(node1, node2, node3,node4):	
 	a="$acc40node1$"
@@ -60,8 +60,8 @@ casestr = inputarr[1]
 if casestr=="genesis":
 	genesis(inputarr[2], inputarr[3], inputarr[4],inputarr[5])
 elif casestr=="passfile":
-   	y=0
-   	for x in range(1,int(inputarr[2])+1,1):
+	y=0
+	for x in range(1,int(inputarr[2])+1,1):
 		y=y+1
 		infile="passfile"+str(y)+".txt"
 		print("creating "+infile)
