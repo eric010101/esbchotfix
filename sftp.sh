eacf670283a953dc5feede74f03f7993bd5e4c45
eric010101 <43527995+eric010101@users.noreply.github.com>
2019-04-14 07:26:57 +0800

    Create sftp.sh

---
+++ sftp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo addgroup sftp-users
+sudo chmod +x sftp.pl
+sudo expect sftp.pl $1 $2
+sudo usermod -G sftp-users -s /bin/false $1
+sudo addgroup ssh-users
+sudo usermod -a -G ssh-users root
+sudo mkdir /var/www
+sudo mkdir /var/www/html
+sudo chown root:sftp-users /var/www/html
+sudo chmod 770 /var/www/html
+
+echo -e "done with installing sftp \n"
