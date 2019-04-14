ab1a048f04785e141c3a0fd816d7643ca84aa4d4
eric010101 <43527995+eric010101@users.noreply.github.com>
2019-04-14 18:29:59 +0800

    Update sftp.sh

--- sftp.sh
+++ sftp.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-sudo apt-get install vsftpd -y
-sudo systemctl start vsftpd
-sudo systemctl enable vsftpd
-sudo addgroup sftp-users
+sudo addgroup esbc
 sudo chmod +x sftp.pl
 sudo expect sftp.pl $1 $2
-sudo usermod -G sftp-users -s /bin/false $1
+sudo usermod -G esbc -s /bin/false $1
 sudo addgroup ssh-users
 sudo usermod -a -G ssh-users root
 sudo mkdir /var/www
 sudo mkdir /var/www/html
-sudo chown root:sftp-users /var/www/html
+sudo chown root:esbc /var/www/html
 sudo chmod 770 /var/www/html
-
+sudo cp sshd_config /etc/ssh/sshd_config
 echo -e "done with installing sftp \n"
