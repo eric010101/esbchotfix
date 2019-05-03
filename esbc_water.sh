dbd8e4b0ca0f327f178d4c32a39aa28f8a9786bb
eric010101 <43527995+eric010101@users.noreply.github.com>
2019-05-03 10:24:25 +0800

    Update esbc_water.sh

--- esbc_water.sh
+++ esbc_water.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo mysql -u root -psweetekhappy -e "create database esbc_water; GRANT ALL PRIVILEGES ON esbc_water.* TO ESBCadmin@localhost IDENTIFIED BY 'ESBChappy'"
+sudo mysql -u root -psweetekhappy -e "GRANT ALL PRIVILEGES ON esbc_water.* TO ZHadmin@localhost IDENTIFIED BY 'ZHhappy'"
 sudo mysql -u root -psweetekhappy esbc_water < esbc_water.sql
