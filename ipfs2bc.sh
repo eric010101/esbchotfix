037b62bb5c937a95842bcc8282c0184b18bba687
eric010101 <43527995+eric010101@users.noreply.github.com>
2020-02-03 17:40:14 +0800

    Update ipfs2bc.sh

--- ipfs2bc.sh
+++ ipfs2bc.sh
@@ -2,7 +2,7 @@
 cd ipfs2bc
 sudo expect ipfs2bc.pl
 npm install moment --save
-npm install --save ipfs-http-client
+npm install --save ipfs-http-client@38.0.0
 npm install express --save
 npm install fs --save
 npm install multer --save
