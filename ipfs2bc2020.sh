71992e15be472db16dde09802d2869d36b8d32cd
eric010101 <43527995+eric010101@users.noreply.github.com>
2020-02-03 19:35:29 +0800

    Create ipfs2bc2020.sh

---
+++ ipfs2bc2020.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ipfs2bc
+sudo expect ipfs2bc.pl
+npm install moment --save
+npm install --save ipfs-http-client@38.0.0
+npm install express --save
+npm install fs --save
+npm install multer --save
+npm install body-parser --save
+npm install baidu-aip-sdk --save
