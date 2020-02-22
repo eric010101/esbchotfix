ad31ab11555d8d2214f9f07442abb498ebf55d24
eric010101 <43527995+eric010101@users.noreply.github.com>
2020-02-22 09:31:48 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,28 +1,28 @@
 <?php
 		
-$SSH_ip= $_GET['ip'];
+$SSH_ip= $_SERVER['SERVER_ADDR'];
 
-?>	
-	<a href="http://equalsmart.com/">equalsmart.com</a>
-	<a href="http://"<? echo $SSH_ip ?>"/phpweb3/esbc_public/">explorer</a>
-	<a href="http://"<? echo $SSH_ip ?>"/phpweb3/esbc_public/home.php">block list</a>
-	<a href="http://"<? echo $SSH_ip ?>"/phpweb3/esbc/">block adm</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog/">VERITAS</a>
-	<a href="http://"<? echo $SSH_ip ?>"/shop/">shop</a>
-	<a href="http://"<? echo $SSH_ip ?>"/cart/">cart</a>
-	<a href="http://"<? echo $SSH_ip ?>"/zdao01/">ERP</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog2/">陳楚彬</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog3/">Wallet</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog4/">法顧平台</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog5/">農業平台</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog6/">健康平台</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog7/">造鏈APP</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog8/">能源平台</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog9/">捐款平台</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog10/">區塊鏈學園</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog11/">copywriter</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog12/">區塊鏈作家</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog13/">區塊链餐廳</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog14/">區塊鏈實驗室</a>
-	<a href="http://"<? echo $SSH_ip ?>"/blog15/">區塊鏈食材</a>
-
+	
+	echo '<a href="http://equalsmart.com/">equalsmart.com</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/phpweb3/esbc_public/">explorer</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/phpweb3/esbc_public/home.php">block list</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/phpweb3/esbc/">block adm</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog/">VERITAS</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/shop/">shop</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/cart/">cart</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/zdao01/">ERP</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog2/">陳楚彬</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog3/">Wallet</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog4/">法顧平台</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog5/">農業平台</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog6/">健康平台</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog7/">造鏈APP</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog8/">能源平台</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog9/">捐款平台</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog10/">區塊鏈學園</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog11/">copywriter</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog12/">區塊鏈作家</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog13/">區塊链餐廳</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog14/">區塊鏈實驗室</a><BR>';
+	echo '<a href="http://'. $SSH_ip.'/blog15/">區塊鏈食材</a><BR>';
+?>
